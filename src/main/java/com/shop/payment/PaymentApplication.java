96e9cc6e452bab66495979aedef00e33dd523ffc
Mandervain <kmnski.l@gmail.com>
2026-06-29 17:37:14 +0200

    feat: add startup logging to all service Application classes

--- src/main/java/com/shop/payment/PaymentApplication.java
+++ src/main/java/com/shop/payment/PaymentApplication.java
@@ -1,12 +1,17 @@
 package com.shop.payment;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PaymentApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(PaymentApplication.class);
+
     public static void main(String[] args) {
+        log.info("Starting shop-payment service");
         SpringApplication.run(PaymentApplication.class, args);
     }
 }
